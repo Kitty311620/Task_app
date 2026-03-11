a85201923ebce8efc522260407b6ca2cb5918bda
unknown <Terryanne.ndululu@gmail.com>
2026-03-11 17:35:04 +0300

    App

--- main.py
+++ main.py
@@ -1,32 +1,20 @@
 import flask
-import random
-app = flask.Flask("MyfirstApp")
-correctnumber = random.randint(0,100)
 
-# homepage,about
-#we use function/methods to make different pages
-@app.route("/homepage")
-def Homepage():
-    return flask.render_template ("Homepage.html")
+app = flask.Flask("To_do_app")
+AllTasks = ["Wash the dishes","Finish homework"]
 
-@app.route("/Aboutme")
-def Aboutme():
-    return flask.render_template ("Aboutme.html")
-@app.route("/Login", methods=["POST", "GET"])
-def Login():
-    return flask.render_template ("Login.html")
-@app.route("/Guess", methods=["POST", "GET"])
-def Guess():
+@app.route("/Tasks", methods=["POST", "GET"])
+def Tasks():
     if flask.request.method == "POST":
-        print ("Someone pressed the button")
-        guessednumber = flask.request.form.get("NO.")
-        print(guessednumber)
-        if int(guessednumber) < correctnumber:
-            return("Too low. Try again")
-        if int(guessednumber) > correctnumber:
-            return("Too high. Try again")
-        if guessednumber == str(correctnumber):
-            return("Congratulations! You guessed the correct number!")
-    return flask.render_template ("Guess.html")
-    
+        Task = flask.request.form.get("Task")
+        print(Task)
+        AllTasks.remove(Task)
+    return flask.render_template ("Tasks.html",AllTasks = AllTasks)
+@app.route("/Addtasks", methods=["POST", "GET"])
+def addTasks():
+    if flask.request.method == "POST":
+        Task = flask.request.form.get("Task")
+        print(Task)
+        AllTasks.append(Task)
+    return flask.render_template ("Addtasks.html")
 #app.run()
